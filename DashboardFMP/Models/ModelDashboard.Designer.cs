916f42b76bb4308791b5a1d6827e386b72bc77f8
Antonio Méndez <jmancio@hotmailcom>
2018-07-05 16:14:38 -0600

    - Borrar proyecto anterior AD - AM 20180705_1615

--- DashboardFMP/Models/ModelDashboard.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'C:\Users\jdonis\Documents\Visual Studio 2012\Projects\DashboardFMP\Dashboard\DashboardFMP\Models\ModelDashboard.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
