515ecbfb9f6eddc28b7c641b6a4ac4f10b44f9a7
Alejandro Donis <alejandro.donis@gmail.com>
2018-07-06 18:09:22 -0400

    Archivos Referencia

--- Dashboard/Models/DashboardModel.Designer.cs
+++ Dashboard/Models/DashboardModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\AD\Dashboard\Dashboard\Models\DashboardModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\jdonis\Documents\Visual Studio 2012\Projects\DashboardFMP\Dashboard\Dashboard\Models\DashboardModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
